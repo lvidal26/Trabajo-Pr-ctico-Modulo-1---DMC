4ae18e6654a8dabc0110156790ce7c57320fbf30
lvidal26 <luisvidal12336@gmail.com>
2026-09-19 17:30:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np
 import libreria_funciones_proyecto1 as lf
 
+
 # CREAR LA NAVEGACIÓN
 pagina = st.sidebar.selectbox(
     "Menú",
@@ -12,7 +13,21 @@ pagina = st.sidebar.selectbox(
 # HOME
 if pagina == "Home":
     st.title("Home")
-    st.write("Bienvenido al proyecto")
+    st.image("logo dmc.png", width = 300)
+    st.sidebar.image("logo dmc.png")
+    st.write("Bienvenido al proyecto Módulo 1 – Python Fundamentals")
+    st.write("**Nombre:** Luis Vidal")
+    st.write("**Edad:** 18 años")
+    st.write("**Ubicación:** Lima")
+    st.write("**Año:** 2026")
+    
+    st.divider()
+    
+    st.write("**Descripción:** Proyecto de Streamlit con 4 ejercicios de Python")
+    
+    st.divider()
+    
+    st.write("**Tecnologías:** Python • Streamlit • Pandas • NumPy")
 
 # EJERCICIO 1
 elif pagina == "Ejercicio 1":
