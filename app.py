d2a68718e903f67ad88daabe50926ade99e93da2
lvidal26 <luisvidal12336@gmail.com>
2026-09-22 02:40:58 -0500

    Rename import for libreria_funciones_proyecto1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
-import libreria_funciones_proyecto1 as lf_fun
+import libreria_funciones_proyecto1 as lf
+import librería_clases_proyecto1 as lf_clase
 import pandas as pd
 
 # CREAR LA NAVEGACIÓN
