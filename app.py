3ac5ed912969263f11695d43578f03b8042873a2
lvidal26 <luisvidal12336@gmail.com>
2026-09-22 19:57:21 -0500

    Fix import statement for libreria_clases_proyecto1

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import numpy as np
 import libreria_funciones_proyecto1 as lf
-import librería_clases_proyecto1 as lf_clase
+import libreria_clases_proyecto1 as lf_clase
 import pandas as pd
-
 
 pagina = st.sidebar.selectbox(
     "Menú",
